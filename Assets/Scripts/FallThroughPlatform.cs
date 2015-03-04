471b4d71cb6141d8ba97bf0a05aa64f2ced0e21f
mrrush2 <michaelrush1@gmail.com>
2015-03-03 20:40:22 -0600

    Can now fall through platforms

---
+++ Assets/Scripts/FallThroughPlatform.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FallThroughPlatform : MonoBehaviour {
+	public GameObject player;
+	bool plat = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnTriggerEnter2D(Collider2D player)
+	{
+		Debug.Log ("On platform");
+	}
+	void OnTriggerStay2D(Collider2D player)
+	{
+		if (Input.GetKey (KeyCode.S)) // This is for falling through the platform
+		{
+			plat = true;
+		}	
+		if (plat) 
+		{
+			Physics2D.IgnoreCollision (collider2D, player, true);
+		}
+	}
+	void OnTriggerExit2D(Collider2D player)
+	{
+		Debug.Log ("Fell through platform");
+		Physics2D.IgnoreCollision (collider2D, player, false);
+		plat = false;
+	}
+}
