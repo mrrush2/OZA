c7ce3c496913bef6c55d692979247c6739d1fdb2
ehnoh2 <ehnoh2@illinois.edu>
2015-03-05 19:13:40 -0600

    Basic AI Follow Script.
    It's a work in progress.

---
+++ Assets/Scripts/ZombieFollow.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ZombieFollow : MonoBehaviour {
+	public Transform target;//set target from inspector instead of looking in Update
+	public float speed = .5f;
+
+	void Update() {
+		//rotate to look at the player
+		target = GameObject.FindWithTag ("Player").transform;
+		Vector3 forwardMovement = new Vector3 (0, 0, -1);
+		transform.LookAt(target.position);
+		//correcting the original rotation
+		transform.Rotate(new Vector3(0,-90,0));
+			transform.position += (transform.TransformDirection (Vector2.right) * speed * Time.deltaTime) * 10;
+
+	}
+
+}
