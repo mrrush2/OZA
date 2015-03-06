8a3c557657ddd4cb35bc520a24ecffdf5a6ae12a
Nick <nickolenz@comcast.net>
2015-03-06 13:23:47 -0600

    Partial implementation of falling platforms.

---
+++ Assets/Scripts/FallingPlatform.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FallingPlatform : MonoBehaviour {
+
+	float originX;
+	float originY;
+
+	// Use this for initialization
+	void Start () 
+	{
+
+		originX = transform.position.x; //Get initial positions
+		originY = transform.position.y;
+
+	}
+	
+	// Update is called constantly
+	void Update () 
+	{
+		transform.Translate (originX - transform.position.x, 0, 0); //Locks the X-axis of the platform
+		if (rigidbody2D.velocity.y < 0)
+			rigidbody2D.AddForce (new Vector2(0, -5));
+	}
+
+	// FixedUpdate is called once per frame
+	void FixedUpdate ()
+	{
+
+	}
+
+}
