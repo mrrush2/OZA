abadef99888dbcd2b6530a9bc83d1380a8c610c3
Nick Olenz <nickolenz@comcast.net>
2015-03-06 19:51:13 -0600

    Prefab'd falling platform.

--- Assets/Scripts/FallingPlatform.cs
+++ Assets/Scripts/FallingPlatform.cs
@@ -20,7 +20,7 @@ public class FallingPlatform : MonoBehaviour {
 	{
 		transform.Translate (originX - transform.position.x, 0, 0); //Locks the X-axis of the platform
 		if (rigidbody2D.velocity.y < 0)
-			rigidbody2D.AddForce (new Vector2(0, -5));
+			rigidbody2D.AddForce (new Vector2(0, -20));
 	}
 
 	// FixedUpdate is called once per frame
