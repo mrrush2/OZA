c83267a9059af1dedf4eff130da88d3d16901179
Nick <nickolenz@comcast.net>
2015-03-08 14:44:51 -0500

    Updated prefabs. Modified falling platform.

--- Assets/Scripts/FallingPlatform.cs
+++ Assets/Scripts/FallingPlatform.cs
@@ -5,6 +5,11 @@ public class FallingPlatform : MonoBehaviour {
 
 	float originX;
 	float originY;
+	Transform origin;
+	public Rigidbody2D platform;
+	GameObject platformInstance;
+
+
 
 	// Use this for initialization
 	void Start () 
@@ -12,6 +17,7 @@ public class FallingPlatform : MonoBehaviour {
 
 		originX = transform.position.x; //Get initial positions
 		originY = transform.position.y;
+		origin = this.transform;
 
 	}
 	
@@ -19,8 +25,16 @@ public class FallingPlatform : MonoBehaviour {
 	void Update () 
 	{
 		transform.Translate (originX - transform.position.x, 0, 0); //Locks the X-axis of the platform
-		if (rigidbody2D.velocity.y < 0)
+		if (rigidbody2D.velocity.y < 0) // Fake Gravity
 			rigidbody2D.AddForce (new Vector2(0, -20));
+/*		if (rigidbody2D.velocity.y < -1) 
+		{
+			Destroy (this.gameObject);
+			platformInstance = Instantiate ( platform, new Vector3(originX, originY, 0), origin.rotation) as GameObject;
+			platformInstance.rigidbody2D.velocity = new Vector2(0,0);
+		} 
+*/
+
 	}
 
 	// FixedUpdate is called once per frame
