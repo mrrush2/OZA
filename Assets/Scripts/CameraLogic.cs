d7a6950f5b572c0d6ed5905827fab31532974e95
Nick <nickolenz@comcast.net>
2015-03-08 17:24:18 -0500

    Started Camera logic, fixed menu bug.

---
+++ Assets/Scripts/CameraLogic.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraLogic : MonoBehaviour {
+
+	GameObject player;
+	bool followingPlayer = true;
+
+	// Use this for initialization
+	void Start ()
+	{
+		player = GameObject.FindGameObjectWithTag ("Player");
+	}
+	
+	// Update is called constantly
+	void Update ()
+	{
+		if (followingPlayer)
+		{
+			transform.position = new Vector3(player.transform.position.x, player.transform.position.y, -10);
+		}
+	}
+
+	// FixedUpdate is called once per frame
+	void FixedUpdate()
+	{
+
+	}
+}
