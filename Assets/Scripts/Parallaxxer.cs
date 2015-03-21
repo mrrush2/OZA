6415bbfe26c5cad65b08df6410c7775f1761de44
Nick Olenz <nickolenz@comcast.net>
2015-03-21 13:43:04 -0500

    Simple BG elements. Trying to get parallax working.

---
+++ Assets/Scripts/Parallaxxer.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Parallaxxer : MonoBehaviour {
+
+	public GameObject controller;
+	public int parallaxValue = 1;
+	float newX;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		newX = (-1) * (controller.transform.position.x / parallaxValue);
+		transform.position = new Vector3 (newX, transform.position.y, transform.position.z);
+	}
+}
