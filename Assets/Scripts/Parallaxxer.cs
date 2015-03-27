c0a8dd8c45466491769d47c2fb17f62a22fc274a
Nick Olenz <nickolenz@comcast.net>
2015-03-26 23:11:20 -0500

    Parallax works, but it's messy. Will tweak.

--- Assets/Scripts/Parallaxxer.cs
+++ Assets/Scripts/Parallaxxer.cs
@@ -3,19 +3,42 @@ using System.Collections;
 
 public class Parallaxxer : MonoBehaviour {
 
-	public GameObject controller;
-	public int parallaxValue = 1;
-	float newX;
+	public Transform[] backgrounds;		// Array of elements to parallax
+	private float[] parallaxScales;		// Speeds
+	public float smoothing = 1f;
 
-	// Use this for initialization
-	void Start () {
-	
+	private Transform cam;				// Reference to camera's pos
+	private Vector3 previousCamPos;		// Position of camera a frame ago
+
+	// Called before Start
+	void Awake () {
+		cam = Camera.main.transform;
 	}
 	
-	// Update is called once per frame
+	// Use this for initialization
+	void Start () {
+		previousCamPos = cam.position;
+
+		// Set each parallax speed
+		parallaxScales = new float[backgrounds.Length];
+		for (int i = 0; i < backgrounds.Length; i++) 
+		{
+			parallaxScales[i] = backgrounds[i].position.z*-1;
+		}
+	}
+	
+	// Update is called constantly
 	void Update () 
 	{
-		newX = (-1) * (controller.transform.position.x / parallaxValue);
-		transform.position = new Vector3 (newX, transform.position.y, transform.position.z);
+		// Go through each background and apply a parallax effect.
+		for (int i = 0; i < backgrounds.Length; i++) 
+		{
+			float parallax = (previousCamPos.x - cam.position.x) * parallaxScales[i];
+			float backgroundTargetPosX = backgrounds[i].position.x + parallax;
+			Vector3 backgroundTargetPos = new Vector3 (backgroundTargetPosX, backgrounds[i].position.y, backgrounds[i].position.z);
+			// Fade the difference using god damn magic (Lerp)
+			backgrounds[i].position = Vector3.Lerp(backgrounds[i].position, backgroundTargetPos, smoothing * Time.deltaTime);
+		}
+		previousCamPos = cam.position;
 	}
 }
