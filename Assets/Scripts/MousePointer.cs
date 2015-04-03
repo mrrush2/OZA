ca9c38a683a6362ed9caac33301239aa42db7295
Nick Olenz <nickolenz@comcast.net>
2015-04-02 23:45:09 -0500

    Added custom cursor because fancy.

---
+++ Assets/Scripts/MousePointer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MousePointer : MonoBehaviour {
+
+	public Texture2D cursor;
+
+	int cursorWidth = 32;
+	int cursorHeight = 32;
+
+	void Start () 
+	{
+		Screen.showCursor = false;
+	}
+
+	void OnGUI () 
+	{
+		GUI.DrawTexture (new Rect (Input.mousePosition.x, Screen.height - Input.mousePosition.y, cursorWidth, cursorHeight), cursor);
+	}
+
+	void Update ()
+	{
+		if (Screen.showCursor)
+			Screen.showCursor = false;
+	}
+}
