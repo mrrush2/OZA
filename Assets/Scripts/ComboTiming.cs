1c23acc92bf523fbb7da6de0d6da031bf61db246
Nick Olenz <nickolenz@comcast.net>
2015-04-02 21:46:23 -0500

    Lots of heavy work on Combo Timing.

--- Assets/Scripts/ComboTiming.cs
+++ Assets/Scripts/ComboTiming.cs
@@ -5,20 +5,23 @@ public class ComboTiming : MonoBehaviour {
 
 	GameObject player;						// A reference to the player object.
 	CombatControllerIII combatController;	// A reference to the combat controller script on the player.
+	Combos comboScript;
 
-	public float baseNoteInterval = 0.25f; //15 frames = a quarter second.
+	public float baseNoteInterval = 0.5f; // for the "par time"
 
-	public float firstNoteTime;		// Time when the first note of a combo is played.							ONLY PUBLIC FOR DEBUG
-	public float secondNoteTime;		// Time when the second note of a combo is played.						ONLY PUBLIC FOR DEBUG
-	public float noteInterval;		// Time between the first and second notes, as a baseline for "beats."		ONLY PUBLIC FOR DEBUG
-	float songTime;			// Time it takes to play an entire song.
-	float predictedSongTime;	// Time the script expects a song to take to play based on the note interval
-								// that the player specifies with their first two notes, and the "beats"
-								// defined by the song.
-	float baseTime;			// The "par" time of a song. Based on some standard note Interval and the
-								// total beats in a combo, defined by the combo.
-	float shittynessConstant;	// How far away songTime is from predictedSongTime.
-	float speedBonus;			// How far away songTime is from baseTime (par.)
+	public float firstNoteTime;			// Time when the first note of a combo is played.								ONLY PUBLIC FOR DEBUG
+	public float secondNoteTime;		// Time when the second note of a combo is played.								ONLY PUBLIC FOR DEBUG
+	public float noteInterval;			// Time between the first and second notes, as a baseline for "beats."			ONLY PUBLIC FOR DEBUG
+	public float songTime;				// Time it takes to play an entire song.										YOU GET THE IDEA
+	public float songTimeAccounter = 0;	// Accounts for previous song time.
+	public float predictedSongTime;		// Time the script expects a song to take to play based on the note interval
+										// that the player specifies with their first two notes, and the "beats"
+										// defined by the song.
+	public float baseTime;					// The "par" time of a song. Based on some standard note Interval and the
+										// total beats in a combo, defined by the combo.
+	public float shittynessConstant;		// How far away songTime is from predictedSongTime.
+	public float speedBonus;				// How far away songTime is from baseTime (par.)
+
 
 	private bool firstSet = false;
 	private bool secondSet = false;
@@ -27,19 +30,23 @@ public class ComboTiming : MonoBehaviour {
 	void Awake () {
 		player = GameObject.FindGameObjectWithTag ("Player");
 		combatController = player.GetComponent<CombatControllerIII>();
+		comboScript = player.GetComponent<Combos>();
 	}
 	
 	// Update is called constantly
 	void Update () {
 
-		// Simple reset.
-		if (combatController.songValue.Length == 0) 
+		// Manual Reset
+		if (Input.GetKeyDown (KeyCode.Keypad0) || Input.GetKeyDown (combatController.keyReset)) 
 		{
 			firstSet = false;
 			secondSet = false;
 			firstNoteTime = 0;
 			secondNoteTime = 0;
+			songTime = 0;
+			songTimeAccounter = 0;
 		}
+
 
 		// Sets the time of the first note played.
 		if (combatController.songValue.Length == 1 && !firstSet)
@@ -57,23 +64,46 @@ public class ComboTiming : MonoBehaviour {
 		}
 						
 
-/*					// Requires new variables in the soon-to-exist ComboCheck class.
+					// Requires new variables in the soon-to-exist ComboCheck class.
 
-		if (a combo is completed)
+		if (comboScript.playedCombo)
 		{
-			songTime = Time.time - firstNoteTime;
-			predictedSongTime = noteInterval * songPlayed.intervals;
+			comboScript.playedCombo = false;
+
+			songTimeAccounter = songTime;
+			songTime = Time.time - firstNoteTime - songTimeAccounter;
+			predictedSongTime = noteInterval * comboScript.intervals;
 			shittynessConstant = Mathf.Abs(predictedSongTime - songTime);
 								
 								
-			baseTime = songPlayed.intervals * baseNoteInterval;
+			baseTime = comboScript.intervals * baseNoteInterval;
 			speedBonus = baseTime - songTime;
 			if (speedBonus < 0)
 				speedBonus = 0;
 										
-			dvalue = songPlayed.baseDamage - shittynessConstant + speedBonus;
+			comboScript.Damage = comboScript.Damage - shittynessConstant + speedBonus;
+
+			// Auto Resets
+			if (combatController.songValue.Length == 0) 
+			{
+				firstSet = false;
+				secondSet = false;
+				firstNoteTime = 0;
+				secondNoteTime = 0;
+				songTime = 0;
+				songTimeAccounter = 0;
+			}
+			// Resets
+			/*
+			songTime = 0;
+			predictedSongTime = 0;
+			baseTime = 0;
+			shittynessConstant = 0;
+			speedBonus = 0;
+			*/
+
 		}
-*/
+
 
 	}
 }
--- Assets/Scripts/Combos.cs
+++ Assets/Scripts/Combos.cs
@@ -7,6 +7,10 @@ public class Combos : MonoBehaviour {
 	CombatControllerIII combat;
 
 	public int specialAttackValue = 0;
+	public bool playedCombo = false;
+
+	public int intervals;
+	public float Damage;
 
 	void Awake ()
 	{
@@ -18,22 +22,37 @@ public class Combos : MonoBehaviour {
 	{
 		//Will revamp to get note values like "19 17 15 17 19 19 19 19" && majorKey to make it simpler 
 		if (specialAttackValue == 0 && combat.songValue.Equals ("6545666") && combat.R == 8 && (combat.majorKey)) {  //Mary Had a Little Lamb 1 in the Key of C Major  
-			combat.FireMd ();
-			specialAttackValue++;
+			playedCombo = true;			// Cannot happen inside TimingInfo, must be set before for the timing script to pick up the values.
+			TimingInfo (7, 10f);		// Passes this song's specific info out to be modified by the timing script.
+			combat.FireMd ();			// Fires the projectile. TODO: Push the damage value into it as parameter.
+			specialAttackValue++;		// Keeps track of what part of the combo can be played.
 		}
 		if (specialAttackValue == 1 && combat.songValue.Equals ("6545666" + "555")) {  //Mary Had a Little Lamb 2  
+			playedCombo = true;
+			TimingInfo (4, 10f);
 			combat.FireMd ();
 			specialAttackValue++;
 		}
 		if (specialAttackValue == 2 && combat.songValue.Equals ("6545666" + "555" + "688")) {  //Mary Had a Little Lamb 3  
+			playedCombo = true;
+			TimingInfo (4, 10f);
 			combat.FireMd ();
 			specialAttackValue++;
 		}
 		if (specialAttackValue == 3 && combat.songValue.Equals ("6545666" + "555" + "688" + "6545666655654")) {  //Mary Had a Little Lamb 4  
+			playedCombo = true;
+			TimingInfo (14, 20f);
 			combat.FireMd ();
-			specialAttackValue = 0;
-			combat.songValue = "";
+			specialAttackValue = 0;		// Reset because song is complete.
+			combat.songValue = "";		// Reset this too, same reason.
 		}
 		
+	}
+
+	// For passing a combo's timing info into the ComboTiming script.
+	void TimingInfo(int comboIntervals, float baseDamage)
+	{
+		intervals = comboIntervals;
+		Damage = baseDamage;
 	}
 }
