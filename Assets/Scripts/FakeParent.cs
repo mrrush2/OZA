06a362eb78052f7b21534e02232f917bb8bc6842
Nick Olenz <nickolenz@comcast.net>
2015-04-05 10:26:00 -0500

    Sky BG sprite. FakeParent script updated.

--- Assets/Scripts/FakeParent.cs
+++ Assets/Scripts/FakeParent.cs
@@ -4,6 +4,15 @@ using System.Collections;
 public class FakeParent : MonoBehaviour {
 
 	// This script will "parent" this object to the target object.
+	public bool followX,
+				followY,
+				followZ;
+
+	public float offsetX,
+				 offsetY,
+				 offsetZ;
+
+
 	public GameObject other;
 
 
@@ -15,6 +24,19 @@ public class FakeParent : MonoBehaviour {
 	
 	// Update is called constantly
 	void Update () {
-		transform.position = other.transform.position;
+
+
+		if (followX) 
+		{
+			transform.position = new Vector3(other.transform.position.x + offsetX, this.transform.position.y, this.transform.position.z);
+		}
+		if (followY) 
+		{
+			transform.position = new Vector3(this.transform.position.x, other.transform.position.y + offsetY, this.transform.position.z);
+		}
+		if (followZ) 
+		{
+			transform.position = new Vector3(this.transform.position.x, this.transform.position.y, other.transform.position.z + offsetZ);
+		}
 	}
 }
