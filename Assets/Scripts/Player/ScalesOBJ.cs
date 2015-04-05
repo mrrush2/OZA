f1ded63cf0c971dc4d9117aeb6e73d620d544b66
Nick <nickolenz@comcast.net>
2015-04-05 17:02:03 -0500

    Cleaned + commented scale code.

--- Assets/Scripts/Player/ScalesOBJ.cs
+++ Assets/Scripts/Player/ScalesOBJ.cs
@@ -4,9 +4,9 @@ using System.Collections.Generic;
 
 public class ScalesOBJ : MonoBehaviour 
 {
-	GameObject player;
-	CombatControllerIII combat;
-	public List<Scale> scaleList = new List<Scale>();
+	GameObject player;									// Ref to player
+	CombatControllerIII combat;							// Ref to combat script
+	public List<Scale> scaleList = new List<Scale>();	// The list of all scales unlocked
 
 	void Awake ()
 	{
@@ -19,10 +19,11 @@ public class ScalesOBJ : MonoBehaviour
 
 	public class Scale
 	{
-		public string name;
-		public int root;
+		public string name;		// Instance vars
+		public int root;		
 		public bool major;
 
+		// Constructor for scales
 		public Scale(string name, int root, bool major)
 		{
 			this.name = name;
@@ -38,6 +39,8 @@ public class ScalesOBJ : MonoBehaviour
 	Scale cMajor = new Scale ("C Major", 8, true);
 	Scale cMinor = new Scale ("C Minor", 8, false);
 
+
+	// The change key function
 	public void ChangeKey(Scale scale)
 	{
 		if (scale.major)
--- Assets/Scripts/UI/ScaleButton.cs
+++ Assets/Scripts/UI/ScaleButton.cs
@@ -5,34 +5,34 @@ using UnityEngine.UI;
 public class ScaleButton : MonoBehaviour {
 
 
-	GameObject player;
-	ScalesOBJ scales;
-	ScalesOBJ.Scale scaleToActivate;
+	GameObject player;					// Ref to player
+	ScalesOBJ scales;					// Ref to scale script
+	ScalesOBJ.Scale scaleToActivate;	// Which scale this button activates
 	
-	public Button scaleButton;
-	public string name;
+	public Button scaleButton;			// Ref to this button
+	public string name;					// The name of the scale this button activates.
 	
 	// Use this for initialization
 	void Awake ()
 	{
-		scaleButton = this.GetComponent<Button>();
-		player = GameObject.FindGameObjectWithTag ("Player"); //Initialize player references.
-		scales = player.GetComponent<ScalesOBJ>();
+		scaleButton = this.GetComponent<Button>();				// Init this button ref
+		player = GameObject.FindGameObjectWithTag ("Player"); 	// Init player references
+		scales = player.GetComponent<ScalesOBJ>();				// Init scale script ref
 	}
 	
 	
 	void Start () 
 	{
-		for (int i = 0; i < scales.scaleList.Count; i++)
+		for (int i = 0; i < scales.scaleList.Count; i++)	// For every scale in list
 		{ 
-			if (scales.scaleList[i].name.Equals(name))
-				scaleToActivate = scales.scaleList[i];	
+			if (scales.scaleList[i].name.Equals(name))		// If it's the right scale by name
+				scaleToActivate = scales.scaleList[i];		// Set it
 		}	
 	
 	
-		scaleButton.onClick.AddListener(() => 
+		scaleButton.onClick.AddListener(() => 				// Adds an event to the button
 		{ 
-			scales.ChangeKey (scaleToActivate);
+			scales.ChangeKey (scaleToActivate);				// Changes the k
 		});
 		
 
