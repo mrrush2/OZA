2f2999c52f59a81144ffeb92bce8e193f2bfbb96
aust427 <austriel27@gmail.com>
2015-04-11 01:20:21 -0500

    Added a in the works AggroMusic Script
    -Added .wav files for snaredrum, and kickdrum. For science.
    -Created a new Script called AggroTime. Currently is playing the
    kickdrum all the time at a constant interval. Will try to tie that with
    aggro maybe hopefully.
    -Modified a line in BackgroundMusic2. Yup.

---
+++ Assets/Scripts/AggroTime.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AggroTime : MonoBehaviour {
+
+	public BackgroundMusics2 DelayNote;
+	ZombieFollow Following;
+	public Transform playerposition; 
+	public float currenttime;
+	AudioClip Percuss;
+	
+	// Use this for initialization
+	void Start () {
+		currenttime = 0;
+		Percuss = Resources.Load ("Sounds/Kickdrum") as AudioClip;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+			if (Time.time - currenttime > 0.5){
+				currenttime = Time.time;
+			AudioSource.PlayClipAtPoint(Percuss, playerposition.position);
+			//	GetComponent<BackgroundMusics2> ().NotePlay ();
+			}
+	}
+}
