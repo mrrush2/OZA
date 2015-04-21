263cadd0536634bfdeb3540708b6731e2d9a2d38
Nick Olenz <nickolenz@comcast.net>
2015-04-21 11:06:34 -0500

    Songs' notes are now shown in the menu.

--- Assets/Scripts/UI/SongController.cs
+++ Assets/Scripts/UI/SongController.cs
@@ -4,8 +4,21 @@ using UnityEngine.UI;
 
 public class SongController : MonoBehaviour 
 {
+	GameObject player;
+	SongsOBJ songs;
+	GameObject songController;
+	
 	public GameObject preview;
 	public Text description;
+	Text[] noteTextBoxes;
+
+	void Awake ()
+	{
+		player = GameObject.FindGameObjectWithTag("Player");
+		songs = player.GetComponent<SongsOBJ>();
+		songController = this.gameObject;
+		noteTextBoxes = songController.GetComponentsInChildren<Text>();
+	}
 
 	public void SetInfo(SongsOBJ.Combo newSong)
 	{
@@ -14,8 +27,22 @@ public class SongController : MonoBehaviour
 		
 		preview.GetComponent<Image>().sprite = previewSprite;
 		description.text = newDesc;
+		SetAllSongNotes(newSong);
 	
 	
+	}
+	
+	public void SetAllSongNotes(SongsOBJ.Combo newSong)
+	{
+		string[] notes = songs.getNotesArray(newSong);
+		for (int i = 0; i < noteTextBoxes.Length; i++)	// Clear it first for a fresh state.
+		{
+			noteTextBoxes[i].text = "";
+		}
+		for (int i = 0; i < notes.Length; i++)			// Put in the notes.
+		{
+			noteTextBoxes[i].text = notes[i];
+		}
 	}
 
 }
