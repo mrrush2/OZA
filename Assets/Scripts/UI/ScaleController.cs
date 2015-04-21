7482d6067cbea04e350f055a5f31cf891400c362
Nick Olenz <nickolenz@comcast.net>
2015-04-21 10:20:55 -0500

    Scales display notes in menu.

--- Assets/Scripts/UI/ScaleController.cs
+++ Assets/Scripts/UI/ScaleController.cs
@@ -4,8 +4,23 @@ using UnityEngine.UI;
 
 public class ScaleController : MonoBehaviour 
 {
+	GameObject player;
+	SongsOBJ songs;
+	GameObject scaleController;
+	Text[] noteTextBoxes;
+
+
 
 	public Text description;
+
+
+	void Awake ()
+	{
+		player = GameObject.FindGameObjectWithTag("Player");
+		songs = player.GetComponent<SongsOBJ>();
+		scaleController = this.gameObject;
+		noteTextBoxes = scaleController.GetComponentsInChildren<Text>();
+	}
 
 
 	public void SetInfo(ScalesOBJ.Scale newScale)
@@ -13,5 +28,16 @@ public class ScaleController : MonoBehaviour
 		string newDesc = newScale.description;
 		
 		description.text = newDesc;
+		SetAllScaleNotes(newScale);
+	}
+	
+	public void SetAllScaleNotes(ScalesOBJ.Scale newScale)
+	{
+		string[] notes = songs.getNotesOfScale(newScale.root, newScale.major);
+		for (int i = 0; i < notes.Length; i ++)
+		{
+			noteTextBoxes[i].text = notes[i]; 
+		}
+		
 	}
 }
