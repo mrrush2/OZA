927abf9424f5b6f71234a56fc080025533a10cff
mrrush2 <michaelrush1@gmail.com>
2015-04-26 18:07:20 -0500

    Line of sight sucks
    Almost have the vertices done

--- Assets/Scripts/Projectiles/MusicNoteSmAttack.cs
+++ Assets/Scripts/Projectiles/MusicNoteSmAttack.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class MusicNoteSmAttack : GenericNoteAttack {
 
 	public override void Customize() {
-		this.setKnockback (250f);
+		this.setKnockback (50f);
 		this.setKnockbackVertical (20f);
 		//this.setGravity (-1f); // For funsies
 	}
