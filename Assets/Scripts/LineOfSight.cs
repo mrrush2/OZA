a5137f2e45440626e1cf70528a5bdc1d37958938
mrrush2 <michaelrush1@gmail.com>
2015-04-26 18:07:41 -0500

    Line of Sight really sucks

--- Assets/Scripts/LineOfSight.cs
+++ Assets/Scripts/LineOfSight.cs
@@ -1,39 +1,57 @@
 using UnityEngine;
 using System.Collections;
-
+using System.Collections.Generic;
 
 
 public class LineOfSight : MonoBehaviour {
-	public Transform display;
+	//public Transform display;
 	//private PolygonCollider2D poly;
 	private float posX;
 	private float posY;
-	private PolygonCollider2D[] allColliders;
-	public Vector2[] allVertices;
+	public PolygonCollider2D[] colliders;
+	public List< Vector2> vertices = new List< Vector2>();
 	public LayerMask blocksInSight;
 	void Start () 
 	{
 		posX = transform.position.x;
 		posY = transform.position.y;
-		allColliders = Physics2D.OverlapCircleAll (Vector2 (posX, posY), 3, blocksInSight, -Mathf.Infinity, Mathf.Infinity);
+		colliders = Physics2D.OverlapCircleAll (new Vector2(posX, posY), 3, blocksInSight, -Mathf.Infinity, Mathf.Infinity);
 		
-	}
+	}	
 	
-	void Update () 
+	void FixedUpdate () 
 	{
+		ArrayList Vetices = new ArrayList();
 		posX = transform.position.x;
 		posY = transform.position.y;
-		allColliders = Physics2D.OverlapCircleAll (Vector2 (posX, posY), 3, blocksInSight, -Mathf.Infinity, Mathf.Infinity);
-		allVertices = FindVertices(allColliders, posX, posY);
+		colliders = Physics2D.OverlapCircleAll (new Vector2 (posX, posY), 3, blocksInSight, -Mathf.Infinity, Mathf.Infinity);
+		FindVertices(colliders);
 		//allVertices[] = transform.position.z;
 		//display.position = allVertices[];
+		//Debug.log (colliders);
 	}
 	
-	void FindVertices(PolygonCollider2D[] poly, Vector2 pointX, Vector2 pointY) 
+	void FindVertices(PolygonCollider2D[] poly) 
 	{
 		for(int i = 0; i < poly.GetLength(); i++)
-		{
-			allVertices[] = poly[i].points;
+		{ 
+			if(poly[i] == PolygonCollider2D)
+			{
+				Vector2[] temp = poly[i].points;
+				for(int j = 0; j < temp.Length; j++)
+				{
+					vertices.Add( temp[j]);
+				}
+			}
+			else if(poly[i] == BoxCollider2D)
+			{
+
+				Vector2[] temp = ;
+				for(int j = 0; j < temp.Length; j++)
+				{
+					vertices.Add( temp[j]);
+				}
+			}
 		}
 
 	}
